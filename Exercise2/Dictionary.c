528801c6e52bbc3fcfea3298b594964e1878daba
Bruno <fbruno017@gmail.com>
2024-01-02 18:53:27 +0000

    Update

--- Exercise2/Dictionary.c
+++ Exercise2/Dictionary.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "Dictionary.h"
+#include "structs.h"
 
 #define LINESIZE 256
 
@@ -12,10 +12,10 @@ void dictionary_destroy(Dictionary *dictionary);
 
 // Function to create a new dictionary using malloc *verify Glib library structures to create a new dictionary and use the correct functions*
 Dictionary *dictionary_create(){
-	Dictionary *dictionary = (Dictionary *)malloc((sizeof(Dictionary)));
-	dictionary -> hash_table = g_hash_table_new(g_str_hash, g_str_equal);
+	Dictionary *dictionary = g_malloc((sizeof(Dictionary)));
+	dictionary -> hash_table = g_hash_table_new_full(g_str_hash, g_str_equal, g_free, NULL);
 	return dictionary;	
-	}
+}
 
 /*
  / Function to add a word to the dictionary
@@ -26,38 +26,43 @@ void dictionary_add(Dictionary *dictionary, const char *filename){
     FILE *file = fopen(filename, "r");
     if (file == NULL){
         printf("Error opening file %s\n", filename);
-        return;
+        exit(EXIT_FAILURE);
     }
-
     char words[LINESIZE];
     const char delims[] = " \n\t\r\v";
-     
-     while(fgets(words,sizeof(words),file) != NULL){
+
+    while(fgets(words, sizeof(words), file)){
         char *word = strtok(words, delims);
         while(word != NULL){
-            g_hash_table_insert(dictionary -> hash_table , word, NULL);
-            word = strtok(NULL, delims);
+            g_hash_table_insert(dictionary->hash_table, g_strdup(word), NULL);
+            word = strtok(NULL, " \n\t\r");
         }
     }
     fclose(file);
-    return;
 }
 // Function to lookup a word in the dictionary, if the word is in the dictionary return 1, else return 0
 
 int dictionary_lookup(Dictionary *dictionary, const char *word){
-    //printf("%d\n", g_hash_table_size(dictionary -> hash_table));
-    if(g_hash_table_contains(dictionary -> hash_table , word) == TRUE){
-        return TRUE;
-    }
-    //printf("FALSEEEEEEEEEEEE\n");
-    return FALSE;
+    return g_hash_table_contains(dictionary -> hash_table , word);
 }
+
 
 // Function to destroy the dictionary -> free all the memory of the dictionary also using malloc
 void dictionary_destroy(Dictionary *dictionary){
-    g_hash_table_remove_all(dictionary -> hash_table);
-    free(dictionary);
-    return;
+    g_hash_table_destroy(dictionary -> hash_table);
+    g_free(dictionary);
+}
+
+
+GList *dictionary_get(Dictionary *dictionary, const char * key) {
+    return g_hash_table_lookup(dictionary -> hash_table, key);
+}
+
+void dictionary_add_word(Dictionary *dictionary, const char * key, Position *position) {
+    GList * value = dictionary_get(dictionary, key);
+    value = g_list_append(value, position);
+    char * key_copy = g_strdup(key);
+    g_hash_table_insert(dictionary -> hash_table, key_copy, value);
 }
 
 // Glib library -> https://docs.gtk.org/glib/
